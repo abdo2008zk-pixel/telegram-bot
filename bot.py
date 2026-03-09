d0b3cb39032bbb5f8a58c165dc02bb35299c6d09
abdo2008zk-pixel <abdo2008.zk@gmail.com>
2026-03-09 13:09:35 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,15 @@
+import telebot
+
+TOKEN =  "8572750379:AAFX2M1zMOPbwRukw09xMSNvem9gJk4W6cM"
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "مرحبا بك في البوت 🤖")
+
+@bot.message_handler(func=lambda m: True)
+def echo(message):
+    bot.reply_to(message, message.text)
+
+bot.infinity_polling()
